2428b05d2c2145d00ad93df9636a0d95a33efb97
kellymw98 <kellymw98@gmail.com>
2019-11-28 16:06:41 -0500

    Add files via upload

---
+++ models/Restaurant.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var reviewSchema = new mongoose.Schema({});
+var menuSchema = new mongoose.Schema({});
+
+var reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 0.0,
+        max: 5.0,
+        required: true
+    },
+    comment: {
+        type: String
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+var menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+});
+
+var restaurantSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    food: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        min: 0,
+        max: 3,
+        required: true
+    },
+    menu: [menuSchema],
+    reviews: [reviewSchema]
+});
+
+var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+module.exports = Restaurant;
